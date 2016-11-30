6cfe3b7bc7cef15235287fe59da19a9bb094e236
pedRo-shd <pedrocustodio@outlook.com.br>
2016-11-30 21:35:44 -0200

    Cadastro Categorias (Backoffice) P.4/4

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
 
   def index
   	@categories = Category.all
@@ -21,10 +23,19 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def update
+    if @category.update(params_category)
+        redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi cadastrada com sucesso!"
+    else
+      render :edit
+    end
   end
 
 
   private
+
+  def set_category
+     @category = Category.find(params[:id])
+  end
 
   def params_category
     params.require(:category).permit(:description)
