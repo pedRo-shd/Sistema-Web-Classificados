d36fdafe3ad66146426b04f3b16203cd044e5142
pedRo-shd <pedrocustodio@outlook.com.br>
2016-12-05 23:18:25 -0200

    Novos Campos Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,8 @@ puts "CATEGORIAS cadastradas com sucesso!"
 ###############################################
 
 puts "Cadastrando o Administrador Padrão..."
-	Admin.create!(email: "admin@admin.com", password: "123456",
-					password_confirmation: "123456")
+	Admin.create!(name: "Administrador Master",
+		email: "admin@admin.com", password: "123456",
+		password_confirmation: "123456")
 
 puts "Administrador cadastrado com sucesso!"
