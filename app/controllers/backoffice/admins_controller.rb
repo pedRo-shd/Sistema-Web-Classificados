855ee0899d401fa5911270b5a43fbd59f4de0f81
pedRo-shd <pedrocustodio@outlook.com.br>
2016-12-12 23:26:30 -0200

    Hash, Params, Strings p/ Password Form

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -24,13 +24,6 @@ end
   end
 
   def update
-    passwd = params[:admin][:password]
-    passwd_confirmation = params[:admin][:password_confirmation]
-
-    if passwd.blank? && passwd_confirmation.blank?
-      params[:admin].delete(:password)
-      params[:admin].delete(:password_confirmation)
-    end
 
     if @admin.update(params_admin)
         redirect_to backoffice_admins_path, notice: "O administrador (#{@admin.email}) foi editado com sucesso!"
@@ -55,6 +48,12 @@ private
   end
 
 def params_admin
+  passwd = params[:admin][:password]
+    passwd_confirmation = params[:admin][:password_confirmation]
+
+    if passwd.blank? && passwd_confirmation.blank?
+      params[:admin].except!(:password, :password_confirmation)
+    end
 	params.require(:admin).permit(:name, :email, :password, :password_confirmation)
 end
 
