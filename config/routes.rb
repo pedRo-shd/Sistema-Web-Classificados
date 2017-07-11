5a6437018d3f2a985613db1153bfc0c6bfed0702
pedRo-shd <pedrocustodio@outlook.com.br>
2017-07-10 23:40:44 -0300

    Controller profile, link para layout profile, ajustando herança de controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
-  resources :send_mail, only: [:edit, :create]
-  resources :categories, except: [:show, :destroy]
-  resources :admins, except: [:show]
-  get 'dashboard', to: 'dashboard#index'
+    resources :send_mail, only: [:edit, :create]
+    resources :categories, except: [:show, :destroy]
+    resources :admins, except: [:show]
+    get 'dashboard', to: 'dashboard#index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
+
+    namespace :profile do
+      resources 'dashboard', only: [:index]
+    end
+
   end
+
 
   devise_for :admins, :skip => [:registrations]
   devise_for :members
