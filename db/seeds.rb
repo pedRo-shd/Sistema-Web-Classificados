60382c6123c6b35c140578ebfee1628cd43ef326
pedRo-shd <pedrocustodio@outlook.com.br>
2017-07-10 23:06:41 -0300

    Configurando devise para members

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 puts "Cadastrando as CATEGORIAS..."
 
-categories = [	"Animais e acessórios",
-				"Esportes",
-				"Para a sua casa",
-				"Eletrônicos e celulares",
-				"Música e hobbies",
-				"Bebês e crianças",
-				"Moda e beleza",
-				"Veículos e barcos",
-				"Imóveis",
-				"Empregos e negócios",	]
-categories.each do | category |
-	Category.find_or_create_by!(description: category)
+categories = [ "Animais e acessórios" ,
+               "Esportes" ,
+               "Para a sua casa" ,
+               "Eletrônicos e celulares" ,
+               "Música e hobbies" ,
+               "Bebês e crianças" ,
+               "Moda e beleza" ,
+               "Veículos e barcos" ,
+               "Imóveis" ,
+               "Empregos e negócios" ]
+
+categories.each do |category|
+  Category.find_or_create_by(description: category)
 end
 
 puts "CATEGORIAS cadastradas com sucesso!"
 
-###############################################
+###################
 
-puts "Cadastrando o Administrador Padrão..."
-	Admin.create!(name: "Administrador Master",
-		email: "admin@admin.com", password: "123456",
-		password_confirmation: "123456",
-		role: 0)
+puts "Cadastrando o ADMINISTRADOR Padrão..."
 
-puts "Administrador cadastrado com sucesso!"
+Admin.create!(
+  name: "Administrador Geral",
+  email: "admin@admin.com",
+  password: "123456",
+  password_confirmation: "123456",
+  role: 0
+)
+
+puts "ADMINISTRADOR cadastrado com sucesso!"
+
+###################
+
+puts "Cadastrando o MEMBRO Padrão..."
+
+Member.create!(
+  email: "membro@membro.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
+puts "MEMBRO cadastrado com sucesso!"
