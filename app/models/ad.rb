3cf112be9fbbc73132a62504d511237c623b8a62
pedRo-shd <pedrocustodio@outlook.com.br>
2017-07-27 22:50:14 -0300

    Editando anúncios P/1

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -5,8 +5,8 @@ class Ad < ActiveRecord::Base
   # gem money-rails
   monetize :price_cents
 
-  scope :to_the, -> (member) { where(member: member)}
-  scope :descending_order, -> (quantity = 10) {limit(6).order(created_at: :desc)}}
+  scope :to_the, -> (member) { where(member: member) }
+  scope :descending_order, -> (quantity = 10) { limit(quantity).order(created_at: :desc) }
 
   # méthod paperclip, picture is name by column in table Ad
   has_attached_file :picture, styles: { medium: "300x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
     namespace :profile do
       resources :dashboard, only: [:index]
-      resources :ads, only: [:index]
+      resources :ads, only: [:index, :edit, :update]
     end
 
   end
