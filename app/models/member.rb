0dae1d5ac1234d7af86b7ddeaa837c8c621ef7f4
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-18 19:42:54 -0300

    Nested Attributes com Devise

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,8 @@ class Member < ActiveRecord::Base
 
   has_many :ads
   has_one :profile_member
-
+  accepts_nested_attributes_for :profile_member
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,10 @@ Rails.application.routes.draw do
 
 
   devise_for :admins, :skip => [:registrations]
-  devise_for :members, controllers: { sessions: 'members/sessions' }
+  devise_for :members, controllers: {
+    sessions: 'members/sessions',
+    registrations: 'members/registrations'
+  }
 
 
   root 'site/home#index'
