21f150df247c726765d6529a236f314cf0bf6e82
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-18 23:08:18 -0300

    Ativando notificações para o cadastro e login de membros, incluindo mensagens bootstrap notify sobre mensagem padrão do devise

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,7 +6,10 @@ class Member < ActiveRecord::Base
   has_many :ads
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
-  
+
+  # Validations
+  # validate :nested_attributes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
