c9d071b7b4524b9cc19b820632046ecf88e48c60
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-25 22:13:18 -0300

    Método para validar first_name e second_name de membro

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,10 +8,20 @@ class Member < ActiveRecord::Base
   accepts_nested_attributes_for :profile_member
 
   # Validations
-  # validate :nested_attributes
+  validate :nested_attributes
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def nested_attributes
+   if nested_attributes_blank?
+      errors.add(:base, "É necessário preencher os campos Primeiro e Segundo nome.")
+    end
+  end
+
+  def nested_attributes_blank?
+    profile_member.first_name.blank? || profile_member.second_name.blank?
+  end
 end
